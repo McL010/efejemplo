aac3dcc1c3816e07c009c069c8f2cb885cd3f19f
Martin Castillo <ocarinaoftime_010@hotmail.com>
2015-11-06 08:12:59 -0800

    se agregaron deps

--- mibd/demoEF.cs
+++ mibd/demoEF.cs
@@ -10,6 +10,7 @@ namespace Ejercicio01.mibd
     public class demoEF :DbContext
     {
        public DbSet<Empleado> Empleados {get; set;}//instancia para crear tablas dentro de la base de datos
+       public DbSet<Departamento> Departamentos { get; set; }
 
     }
 }
